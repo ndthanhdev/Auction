71e51fe38b0086d6c70675e3b35fdf03fa3ea7bd
DuyThanhNguyen <thanhtdt@outlook.com>
2017-05-03 23:57:46 +0700

    up

---
+++ ActionServer/app/Entities/Bid.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: duyth
+ * Date: 5/2/2017
+ * Time: 3:12 PM
+ */
+
+namespace App\Entities;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bid extends Model
+{
+
+}
